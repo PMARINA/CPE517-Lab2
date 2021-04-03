ba7d58661010cb3fe6864d72b60cbf4c40bfdb92
PMARINA <bluerat33@gmail.com>
2021-04-03 15:17:09 -0400

    Testing bgtz

--- src/sim.c
+++ src/sim.c
@@ -11,9 +11,10 @@
 
 uint8_t op;
 uint32_t instruction;
-uint32_t rs, rt, rd;  // to save the register value
+uint8_t rs, rt, rd; // We only save the register number because it's confusing
+                    // to have copies
 uint8_t func;
-uint16_t itemp;  // for save immidiate number
+uint16_t itemp; // for save immidiate number
 
 // please take a loot at
 // https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_Green_Sheet.pdf
@@ -22,52 +23,55 @@ uint16_t itemp;  // for save immidiate number
 
 void fetch() {
   printf("fetch ");
-  instruction = mem_read_32(CURRENT_STATE.PC);  // the instruction
+  instruction = mem_read_32(CURRENT_STATE.PC); // the instruction
   if (instruction == 0) {
+    // Either there is no instruction that is entirely 0 or we are not required
+    // to implement it. In either case, we rely on the shell.c doing memset with
+    // 0 to ensure we don't read in random garbage.
     RUN_BIT = 0;
   }
   NEXT_STATE.PC = CURRENT_STATE.PC + 4;
 }
 
 void decode() {
-  // printf("decode ");
-  op = instruction >> 26;  // find the 31-26 bit
-  //  printf("the op: %x ",op);
-  // if op==0, it is R type
-  // if I type, there is no funct, so set func based off of opcode
-  if (op == 0) {  // R type instructions
+  printf("decode ");
+  op = instruction >> 26; // find the 31-26 bit
+  printf("the op: %x ", op);
+  if (op == 0) { // R type instructions
     func = instruction & 0x3f;
     rs = (instruction >> 21) & 0x1f;
     rt = (instruction >> 16) & 0x1f;
     rd = (instruction >> 11) & 0x1f;
-  } else {                     // if i != 0, I-type, J-type
-    if (op == 2 || op == 3) {  // J-type
+  } else {                    // if i != 0, I-type, J-type
+    if (op == 2 || op == 3) { // J-type
       // The op is a Jump or JAL operation
-      // Nothing to do if it's a jump
+      // Nothing to do if it's a jump (we don't have an address global variable)
     } else {
       // Any jump instructions not supported (all besides J, JAL) will be
       // misclassified as I type
-      rs = (instruction >> 21) & 0x1f;  // 0-31
+      rs = (instruction >> 21) & 0x1f; // 0-31
       rt = (instruction >> 16) & 0x1f;
       itemp = instruction & (uint32_t)(0xffff);
     }
-    func = op;
+    func = op; // in case we accidentally touch the op, which should be 0.
   }
 }
 
 void execute_addi() {
   printf("addi\n");
-  if (itemp > 32767) {   // 2^15-1=32767 is the largest number
-    itemp = ~itemp + 1;  // 2's complement
+  if (itemp > 32767) {  // 2^15-1=32767 is the largest number
+    itemp = ~itemp + 1; // 2's complement
+    // Taking 2's complement allows you to "negate" a number, thus we take the
+    // very large number (indicative of a negative) number, and negate it and
+    // then subtract it to simulate adding a negative number.
     NEXT_STATE.REGS[rt] = CURRENT_STATE.REGS[rs] - itemp;
   } else {
     NEXT_STATE.REGS[rt] = CURRENT_STATE.REGS[rs] + itemp;
   }
-  // NEXT_STATE.REGS[addr_rt] = rt;  // It is fine if you just execute here
-  // instead of executing in execute() function.
 }
 
 void execute_addiu() {
+  // Add immediate unsigned
   printf("addiu\n");
   NEXT_STATE.REGS[rt] =
       ((uint32_t)(CURRENT_STATE.REGS[rs])) + ((uint32_t)(itemp));
@@ -76,7 +80,16 @@ void execute_addiu() {
 void execute_bgtz() {
   printf("bgtz\n");
   if (CURRENT_STATE.REGS[rs] > 0) {
-    uint32_t addr = CURRENT_STATE.PC + (itemp + 1) * 4;
+    // 0x1d20fffd
+    // i type, opcode = 7
+    // jump back imm * 4
+    // jump forward (imm + 1) * 4
+    uint32_t addr;
+    if (itemp > 32767) {
+      addr = CURRENT_STATE.PC - (~itemp + 1) * 4;
+    } else {
+      addr = CURRENT_STATE.PC + (itemp + 1) * 4;
+    }
     NEXT_STATE.PC = addr;
   }
 }
@@ -90,8 +103,6 @@ void execute_lb() {
     val = (mem_read_32(addr - (itemp + 3) / 4 * 4) >>
            ((4 - (itemp % 4)) % 4) * 8) &
           0xff;
-    // val = (mem_read_32(addr - ((itemp + 3) / 4) * 4) &
-    //        ((0xff << 24 - (((itemp % 4 - 1) * 8)))));
   } else
     val = (mem_read_32(addr + (itemp / 4) * 4) >> ((itemp % 4) * 8) & 0xff);
   if (val > 127) {
@@ -103,7 +114,12 @@ void execute_lb() {
 void execute_lw() {
   printf("lw\n");
   uint32_t addr = CURRENT_STATE.REGS[rs];
-  NEXT_STATE.REGS[rt] = mem_read_32(addr + itemp);
+  if (itemp > 32767) {
+    itemp = ~itemp + 1;
+    NEXT_STATE.REGS[rt] = mem_read_32(addr - itemp);
+  } else {
+    NEXT_STATE.REGS[rt] = mem_read_32(addr + itemp);
+  }
 }
 
 void execute_sb() {
@@ -141,31 +157,48 @@ void execute_sw() {
   uint32_t addr = CURRENT_STATE.REGS[rs];
   if (itemp > 32767) {
     itemp = ~itemp + 1;
+    mem_write_32(addr - itemp, CURRENT_STATE.REGS[rt]);
+  } else {
+    mem_write_32(addr + itemp, CURRENT_STATE.REGS[rt]);
   }
-  mem_write_32(addr, CURRENT_STATE.REGS[rt] + itemp);
 }
 
 void execute_jump() {
   printf("jump\n");
+  // Per the manual, we use the high 4 bits of the pc
+  // and the low 26 bits from the instruction, but shift that left 2 so that the
+  // lsb won't result in a badly aligned address.
+  // Decode the address
   uint32_t addr = ((instruction & 0x3ffffff) << 2);
   addr = addr | (mem_read_32(CURRENT_STATE.PC) & (0xf << 28));
+  // Set the PC to the jump address
   NEXT_STATE.PC = addr;
 }
 
 void execute_jal() {
   printf("jal\n");
+  // Jump and Link
   uint32_t addr = ((instruction & 0x3ffffff) << 2);
+  // We do decoding here to get the address to jump to
   addr += CURRENT_STATE.PC & (0xf << 28);
+  // Set the PC to the jump address
   NEXT_STATE.PC = addr;
+  // Per this extensive documentation, the register 31 is the hardcoded location
+  // of the return address.
+  // https://www.cs.cmu.edu/afs/cs/academic/class/15740-f97/public/doc/mips-isa.pdf
   NEXT_STATE.REGS[31] = CURRENT_STATE.PC + 4;
 }
 
 void execute_jr() {
   printf("jr\n");
+  // Set the PC to the value provided.
   NEXT_STATE.PC = CURRENT_STATE.REGS[rs];
+  // No other action taken.
 }
 
 void execute_lui() {
+  // Load upper immediate
+  // Makes for easier testing since the MIPS compiler uses it a lot
   // 3c 01 10 01
   // 0011 1100 0000 0001 0001 0000 0000 0001
   // ____ __ = 1111 = 15                              OPCODE
@@ -173,111 +206,111 @@ void execute_lui() {
   //              _ ____ = 1                          RT
   //                     ____ ____ ____ ____ = 4097   IMM (itemp)
   printf("lui\n");
-  printf("rt is %d\n", rt);
-  printf("itemp is %04x\n", itemp);
+  // Set the upper bits to whatever is specified. Lower bits are always cleared
+  // out.
   NEXT_STATE.REGS[rt] = itemp << 16;
 }
 
 void execute() {
   printf("\nexecute\t");
-  if (op == 0) {  // R type instruction
+  if (op == 0) { // R type instruction
     printf("R-type\t");
     switch (func) {
-      case 8:
-        // JR = 8 == return
-        execute_jr();
-        break;
-      case 32:  // add:100000
-        // ADD = 32
-        printf("add\n");
-        //      if(rt>2147483647)
-        NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[rt];
-        break;
-      case 33:
-        // ADDU = 33
-        printf("addu\n");
-        NEXT_STATE.REGS[rd] = (uint32_t)(CURRENT_STATE.REGS[rs]) +
-                              (uint32_t)(CURRENT_STATE.REGS[rt]);
-        break;
-      case 34:
-        // SUB = 34
-        printf("sub\n");
-        NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt];
-        break;
-      case 35:
-        // SUBU = 35
-        printf("subu\n");
-        NEXT_STATE.REGS[rd] = (uint32_t)(CURRENT_STATE.REGS[rs]) -
-                              (uint32_t)(CURRENT_STATE.REGS[rt]);
-        break;
-      case 36:
-        // AND = 36
-        printf("and\n");
-        NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] & CURRENT_STATE.REGS[rt];
-        break;
-      case 12:                              // system call:001100
-        if (CURRENT_STATE.REGS[2] == 10) {  // v0==10 then exit
-          printf("systemcall: exit\n");
-          RUN_BIT = FALSE;
-        }
-        if (CURRENT_STATE.REGS[2] == 1) {       // v0==1: print int
-          int32_t val = CURRENT_STATE.REGS[4];  // a0 is 4th register
-          printf("\n print the number:   %d \n ", val);
-        }
-        break;
-      default:
-        break;
+    case 8:
+      // JR = 8 == return
+      execute_jr();
+      break;
+    case 32: // add:100000
+      // ADD = 32
+      printf("add\n");
+      //      if(rt>2147483647)
+      NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[rt];
+      break;
+    case 33:
+      // ADDU = 33
+      printf("addu\n");
+      NEXT_STATE.REGS[rd] = (uint32_t)(CURRENT_STATE.REGS[rs]) +
+                            (uint32_t)(CURRENT_STATE.REGS[rt]);
+      break;
+    case 34:
+      // SUB = 34
+      printf("sub\n");
+      NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt];
+      break;
+    case 35:
+      // SUBU = 35
+      printf("subu\n");
+      NEXT_STATE.REGS[rd] = (uint32_t)(CURRENT_STATE.REGS[rs]) -
+                            (uint32_t)(CURRENT_STATE.REGS[rt]);
+      break;
+    case 36:
+      // AND = 36
+      printf("and\n");
+      NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] & CURRENT_STATE.REGS[rt];
+      break;
+    case 12:                             // system call:001100
+      if (CURRENT_STATE.REGS[2] == 10) { // v0==10 then exit
+        printf("systemcall: exit\n");
+        RUN_BIT = FALSE;
+      }
+      if (CURRENT_STATE.REGS[2] == 1) {      // v0==1: print int
+        int32_t val = CURRENT_STATE.REGS[4]; // a0 is 4th register
+        printf("\n print the number:   %d \n ", val);
+      }
+      break;
+    default:
+      break;
     }
   } else {
     if (op == 2 || op == 3) {
       printf("J-type\t");
       // Execute Jumps
       switch (op) {
-        case 2:
-          // J = 2
-          execute_jump();
-          break;
-        case 3:
-          // JAL = 3 (BONUS)
-          execute_jal();
-          break;
+      case 2:
+        // J = 2
+        execute_jump();
+        break;
+      case 3:
+        // JAL = 3 (BONUS)
+        execute_jal();
+        break;
       }
     } else {
       printf("I-type\t");
       // Execute I-type
       switch (op) {
-        case 7:
-          // BGTZ = 7
-          execute_bgtz();
-          break;
-        case 8:
-          // ADDI = 8
-          execute_addi();
-          break;
-        case 9:
-          // ADDIU = 9
-          execute_addiu();
-          break;
-        case 15:
-          // LUI = 15
-          execute_lui();
-          break;
-        case 32:
-          // LB = 32 (BONUS)
-          execute_lb();
-          break;
-        case 35:
-          // LW = 35
-          execute_lw();
-          break;
-        case 40:
-          // SB = 40 (BONUS)
-          execute_sb();
-          break;
-        case 43:
-          // SW = 43
-          execute_sw();
-          break;
+      case 7:
+        // BGTZ = 7
+        execute_bgtz();
+        break;
+      case 8:
+        // ADDI = 8
+        execute_addi();
+        break;
+      case 9:
+        // ADDIU = 9
+        execute_addiu();
+        break;
+      case 15:
+        // LUI = 15
+        execute_lui();
+        break;
+      case 32:
+        // LB = 32 (BONUS)
+        execute_lb();
+        break;
+      case 35:
+        // LW = 35
+        execute_lw();
+        break;
+      case 40:
+        // SB = 40 (BONUS)
+        execute_sb();
+        break;
+      case 43:
+        // SW = 43
+        execute_sw();
+        break;
       }
     }
   }
@@ -289,7 +322,8 @@ void process_instruction() {
    * access memory.
    * */
   fetch();
-  if (RUN_BIT == 0) return;
+  if (RUN_BIT == 0)
+    return;
   decode();
   execute();
 }
