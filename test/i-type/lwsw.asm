ee08ea611d01740688765e7d1aad7de559d177f0
PMARINA <bluerat33@gmail.com>
2021-04-02 21:30:40 -0400

    mod lwsw test

--- test/i-type/lwsw.asm
+++ test/i-type/lwsw.asm
@@ -1,6 +1,6 @@
-.data
-val: 0xfdfcfbfa
 .text
+addi $t0, $zero, 0xab
+lui $t0, 0xce000000
 lw $t0, val
 addi $t0, $t0, 1
 sw $t0, val
@@ -8,6 +8,17 @@ addi $a0, $zero, 0
 lw $a0, val
 addi $v0, $zero, 1
 syscall
+
+addi $t0, $zero, 0xab
+lui $t0, 0x4e000000
+lw $t0, val
+addi $t0, $t0, 1
+sw $t0, val
+addi $a0, $zero, 0
+lw $a0, val
+addi $v0, $zero, 1
+syscall
+
 addi $v0, $zero, 10
 syscall
 
