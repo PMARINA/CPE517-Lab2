f3b23b69ff13d32a23a0a12062c0272ed8bfb28e
PMARINA <bluerat33@gmail.com>
2021-04-02 22:27:07 -0400

    updated lbsb to test non-32-bit aligned addresses

--- test/i-type/lbsb.asm
+++ test/i-type/lbsb.asm
@@ -1,14 +1,15 @@
 .text
 
 addi $t0, $zero, 0xfa
-sb $t0, 0x10010000
+lui $t1, 0x1001
+sb $t0, 1($t1)
 add $t0, $zero, $zero
 
-lb $t0, 0x10010001
+lb $t0, 1($t1)
 addi $t0, $t0, 1
-sb $t0, 0x10010000
+sb $t0, 1($t1)
 addi $a0, $zero, 0
-lb $a0, 0x10010000
+lb $a0, 1($t1)
 addi $v0, $zero, 1
 syscall
 
