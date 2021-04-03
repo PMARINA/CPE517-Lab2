88408aa48b071dde53ba8a1aee5e2665aa42c2d1
PMARINA <bluerat33@gmail.com>
2021-04-02 23:01:41 -0400

    test all lbsb offset

--- test/i-type/lbsb.asm
+++ test/i-type/lbsb.asm
@@ -6,6 +6,34 @@ addi $t1, $t1, 4
 sb $t0, -1($t1)
 add $t0, $zero, $zero
 
+lb $t0, -1($t1)
+addi $t0, $t0, 1
+sb $t0, -1($t1)
+addi $a0, $zero, 0
+lb $a0, -1($t1)
+addi $v0, $zero, 1
+syscall
+
+addi $t0, $zero, 0xfa
+lui $t1, 0x1001
+addi $t1, $t1, 4
+sb $t0, -1($t1)
+add $t0, $zero, $zero
+
+lb $t0, 1($t1)
+addi $t0, $t0, 1
+sb $t0, 1($t1)
+addi $a0, $zero, 0
+lb $a0, 1($t1)
+addi $v0, $zero, 1
+syscall
+
+addi $t0, $zero, 0xfa
+lui $t1, 0x1001
+addi $t1, $t1, 4
+sb $t0, 1($t1)
+add $t0, $zero, $zero
+
 lb $t0, 1($t1)
 addi $t0, $t0, 1
 sb $t0, 1($t1)
